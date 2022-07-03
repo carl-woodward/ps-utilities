5e0f6465c6db533f6ecaf2892f9c9867d1446e29
Carl Woodward <cdwsignup@gmail.com>
2022-07-03 08:32:07 -0700

    Fixed repo renaming issues.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-Import-Module ~\Source\get-errortext\ps_utilities.psm1
+Import-Module ~\Source\ps-utilities\ps_utilities.psm1
 
 Set-Alias -Name err -Value Get-ErrorText
 Set-Alias -Name ee -Value Format-Integer
